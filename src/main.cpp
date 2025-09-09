39329c2c736662473b305b4c004fee8626eadcbe
ewyrick-mark-1 <elliot@wyrick.ws>
2025-09-09 13:53:24 -0400

    report

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <vector> // Include for std::vector
+#include <vector>
 
 using namespace std;
 
 void SelectionSort(vector<int>& arr, int size){
-    vector<int> sorted(size);
-    for (int i = 0; i <size; i++){
+    
+    for (int i = 0; i <size - 1; i++){
         int min_index = i;
         for(int j = i+1; j < size; j++){
             if(arr[j] < arr[min_index]){
