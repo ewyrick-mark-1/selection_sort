9dd69ddf3122e0441f0c79da14b8988e340df4ff
ewyrick-mark-1 <elliot@wyrick.ws>
2025-09-08 22:09:27 -0400

    prep

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+//just building the repo, starting tomorrow
+int main() {
+    int N;
+    cout << "Enter number of boxes:\n";
+    cin >> N;
+
+    cout << "number entered: " << N << "\n";
+    return 0;
+}
