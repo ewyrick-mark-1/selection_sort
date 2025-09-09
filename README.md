f0f5a6f7c833c0629372784ac78593de46669706
ewyrick-mark-1 <elliot@wyrick.ws>
2025-09-09 12:06:37 -0400

    added sorting functionality

---
+++ README.md
@@ -0,0 +1,14 @@
+Basic integer selection sort program.
+Elliot Wyrick - 9/9/25
+
+To run:
+
+- initize cmake in hom directory                "cmake ."
+- run the makefile it creates                   "make"
+- run the executable made by the makefile       "./SelectionSort"
+
+terminal will prompt for:
+- size of the array to be sorted
+- each individual array element
+
+upon entering all data, a confirmation of the array you entered will be printed, as well as the sorted array.
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
+#include <vector> // Include for std::vector
 
 using namespace std;
-//just building the repo, starting tomorrow
-int main() {
-    int N;
-    cout << "Enter number of boxes:\n";
-    cin >> N;
 
-    cout << "number entered: " << N << "\n";
+void SelectionSort(vector<int>& arr, int size){
+    vector<int> sorted(size);
+    for (int i = 0; i <size; i++){
+        int min_index = i;
+        for(int j = i+1; j < size; j++){
+            if(arr[j] < arr[min_index]){
+                min_index = j;
+            }
+        }
+        if(arr[min_index] < arr[i]){
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+        
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    // Create a vector of the specified size
+    vector<int> arr(size); 
+
+    cout << "Enter integers:\n";
+
+    for (int i = 0; i < size; ++i) { // for loop for taking a vector input
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Array elements are: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+
+        
+    }
+    cout << "\n";
+    SelectionSort(arr, size);
+    cout << "sorted array elements are: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+
+        
+    }
+
+
     return 0;
 }
